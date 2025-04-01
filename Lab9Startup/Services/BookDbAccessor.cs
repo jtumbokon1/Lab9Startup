5ea18dd39a3d825fd211c6c07dcdb11101e3783b
Jirch Tumbokon <jirchgot@gmail.com>
2025-04-01 08:46:41 -0600

    Add project files.

---
+++ Lab9Startup/Services/BookDbAccessor.cs
@@ -0,0 +1,121 @@
+﻿using Dapper;
+using Lab9Startup.Models;
+using MySqlConnector;
+
+namespace Lab9Startup.Services
+{
+    public class BookDbAccessor
+    {
+        protected MySqlConnection connection;
+
+        public BookDbAccessor()
+        {
+            // get environemnt variable
+            //string dbHost = Environment.GetEnvironmentVariable("DB_HOST");
+            //string dbUser = Environment.GetEnvironmentVariable("DB_USER");
+            //string dbPassword = Environment.GetEnvironmentVariable("DB_PASSWORD");
+            string dbHost = "localhost";
+            string dbUser = "root";
+            string dbPassword = "password";
+
+            var builder = new MySqlConnectionStringBuilder
+            {
+                Server = dbHost,
+                UserID = dbUser,
+                Password = dbPassword,
+                Database = "library", // Use maria db to create a database called library
+            };
+
+            connection = new MySqlConnection(builder.ConnectionString);
+        }
+
+        /// <summary>
+        /// Initialize the database and create the books table
+        /// </summary>
+        public void InitializeDatabase()
+        {
+            connection.Open();
+
+            var sql = @"CREATE TABLE IF NOT EXISTS books (
+                BookId VARCHAR(36) PRIMARY KEY,
+                Title VARCHAR(255) NOT NULL,
+                Author VARCHAR(255) NOT NULL,
+                Description TEXT,
+                Category VARCHAR(255)
+            )";
+
+            connection.Execute(sql);
+
+            connection.Close();
+        }
+
+        /// <summary>
+        /// Implement the AddBook method to add a book to the database
+        /// </summary>
+        /// <param name="book"></param>
+        public void AddBook(Book book)
+        {
+            var sql = @"INSERT INTO books (BookId, Title, Author, Description, Category) 
+                VALUES (@BookId, @Title, @Author, @Description, @Category)";
+
+            connection.Open();
+
+            connection.Execute(sql, new
+            {
+                book.BookId,
+                book.Title,
+                book.Author,
+                book.Description,
+                book.Category});
+
+            connection.Close();
+        }
+
+
+        /// <summary>
+        /// Implement the GetBooks method to get all books from the database
+        /// </summary>
+        /// <returns></returns>
+        public List<Book> GetBooks()
+        {
+            var sql = "SELECT * FROM books";
+
+            connection.Open();
+
+            var books = connection.Query<Book>(sql).ToList();
+
+            connection.Close();
+
+            return books;
+        }
+
+
+        /// <summary>
+        /// Implement the GetBook method to get a book from the database
+        /// </summary>
+        /// <param name="bookId"></param>
+        /// <returns></returns>
+        public Book GetBook(string bookId)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Implement the UpdateBook method to update a book in the database
+        /// </summary>
+        /// <param name="book"></param>
+        public void UpdateBook(Book book)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Implement the DeleteBook method to delete a book from the database
+        /// </summary>
+        /// <param name="bookId"></param>
+        public void DeleteBook(string bookId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
